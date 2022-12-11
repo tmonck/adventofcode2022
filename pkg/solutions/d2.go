ee7c62d0b4f367b64c71f7f84b6723c7c6af93c8
Tom Monck <tom@monck.info>
2022-12-11 07:47:49 -0500

    Cleaning up day2 some

--- pkg/solutions/d2.go
+++ pkg/solutions/d2.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Day2 struct {
-	Puzzle               int
+	Day
 	choices              Choices
 	results              Results
 	secondColumnIsChoice bool
@@ -22,33 +22,40 @@ type Choice struct {
 }
 
 func Day2Init(puzzle int) Day2 {
-	day2 := Day2{Puzzle: puzzle}
+	day2 := Day2{}
+	day2.Puzzle = puzzle
+	day2.choices = map[string]Choice{}
+	day2.results = map[string]Result{}
+	day2.secondColumnIsChoice = true
+	currDir, _ := os.Getwd()
+	filepathFmt := "%s/pkg/inputs/day2/%s.txt"
+
+	Rock := Choice{name: "Rock", score: 1, beats: "Scissors", code: []string{"A", "X"}}
+	Paper := Choice{name: "Paper", score: 2, beats: "Rock", code: []string{"B", "Y"}}
+	Scissors := Choice{name: "Scissors", score: 3, beats: "Paper", code: []string{"C", "Z"}}
 	switch puzzle {
-	case 2:
-		fmt.Println("case2")
-		var Rock = Choice{name: "Rock", score: 1, beats: "Scissors", code: []string{"A"}}
-		var Paper = Choice{name: "Paper", score: 2, beats: "Rock", code: []string{"B"}}
-		var Scissors = Choice{name: "Scissors", score: 3, beats: "Paper", code: []string{"C"}}
-		day2.choices = map[string]Choice{}
+	case 1:
 		day2.choices[Rock.name] = Rock
 		day2.choices[Paper.name] = Paper
 		day2.choices[Scissors.name] = Scissors
-		day2.results = map[string]Result{}
+		day2.fileName = fmt.Sprintf(filepathFmt, currDir, "puzzle1")
+	case 2:
+		Rock.code = []string{"A"}
+		Paper.code = []string{"B"}
+		Scissors.code = []string{"C"}
+		day2.choices[Rock.name] = Rock
+		day2.choices[Paper.name] = Paper
+		day2.choices[Scissors.name] = Scissors
 		day2.results["X"] = Result{code: "X", score: 0}
 		day2.results["Y"] = Result{code: "Y", score: 3}
 		day2.results["Z"] = Result{code: "Z", score: 6}
 		day2.secondColumnIsChoice = false
+		day2.fileName = fmt.Sprintf(filepathFmt, currDir, "puzzle1")
 	default:
-		fmt.Println("default")
-		var Rock = Choice{name: "Rock", score: 1, beats: "Scissors", code: []string{"A", "X"}}
-		var Paper = Choice{name: "Paper", score: 2, beats: "Rock", code: []string{"B", "Y"}}
-		var Scissors = Choice{name: "Scissors", score: 3, beats: "Paper", code: []string{"C", "Z"}}
-		day2.choices = map[string]Choice{}
 		day2.choices[Rock.name] = Rock
 		day2.choices[Paper.name] = Paper
 		day2.choices[Scissors.name] = Scissors
-		day2.results = map[string]Result{}
-		day2.secondColumnIsChoice = true
+		day2.fileName = fmt.Sprintf(filepathFmt, currDir, "sample")
 	}
 	return day2
 }
@@ -72,18 +79,9 @@ var results = Results{}
 
 func (d Day2) Run() int {
 	fmt.Println(fmt.Sprintf("Puzzle: %d", d.Puzzle))
-	currDir, _ := os.Getwd()
 	var inputBytes []byte
-	filepathFmt := "%s/pkg/inputs/day2/%s.txt"
 	totalScore := 0
-	switch puzzle := d.Puzzle; puzzle {
-	case 0:
-		fileName := fmt.Sprintf(filepathFmt, currDir, "sample")
-		inputBytes, _ = ioutil.ReadFile(fileName)
-	default:
-		fileName := fmt.Sprintf(filepathFmt, currDir, "puzzle1")
-		inputBytes, _ = ioutil.ReadFile(fileName)
-	}
+	inputBytes, _ = ioutil.ReadFile(d.fileName)
 	totalScore = d.CalculateScore(inputBytes, d.secondColumnIsChoice)
 	return totalScore
 }
