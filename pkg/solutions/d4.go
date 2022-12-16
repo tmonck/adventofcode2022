3a994c31b5c2c1d2369fbb2ba8d8969f684c35a7
Tom Monck <tmonck@bandwidth.com>
2022-12-15 19:07:43 -0500

    Simplification of the code for d4

--- pkg/solutions/d4.go
+++ pkg/solutions/d4.go
@@ -12,6 +12,9 @@ type Day4 struct {
 	Day
 }
 
+type Sections []Section
+type Section []int
+
 func Day4Init(puzzle int) Day4 {
 	day4 := Day4{}
 	day4.Puzzle = puzzle
@@ -28,25 +31,20 @@ func Day4Init(puzzle int) Day4 {
 
 func (d Day4) Run() string {
 	inputBytes := []byte{}
+	inputBytes, _ = ioutil.ReadFile(d.fileName)
 	overlappingSectionsCount := 0
-	switch d.Puzzle {
-	case 2:
-		inputBytes, _ = ioutil.ReadFile(d.fileName)
-		for _, line := range strings.Split(string(inputBytes), "\n") {
-			if len(line) == 0 {
-				continue
-			}
-			if d.DoesAnySectionOverlap(line) {
+	for _, line := range strings.Split(string(inputBytes), "\n") {
+		if len(line) == 0 {
+			continue
+		}
+		sections := d.ParseSections(line)
+		switch d.Puzzle {
+		case 2:
+			if sections.HasAnyOverlap() {
 				overlappingSectionsCount++
 			}
-		}
-	default:
-		inputBytes, _ = ioutil.ReadFile(d.fileName)
-		for _, line := range strings.Split(string(inputBytes), "\n") {
-			if len(line) == 0 {
-				continue
-			}
-			if d.IsOneSectionCompleteOverlap(line) {
+		default:
+			if sections.HasCompleteOverlap() {
 				overlappingSectionsCount++
 			}
 		}
@@ -54,9 +52,8 @@ func (d Day4) Run() string {
 	return strconv.Itoa(overlappingSectionsCount)
 }
 
-func (d Day4) IsOneSectionCompleteOverlap(line string) bool {
+func (d Day4) ParseSections(line string) (results Sections) {
 	sections := strings.Split(line, ",")
-	results := [][]int{}
 	for _, section := range sections {
 		tempStrSlice := strings.Split(section, "-")
 		tempIntSlice := []int{}
@@ -66,63 +63,29 @@ func (d Day4) IsOneSectionCompleteOverlap(line string) bool {
 		}
 		results = append(results, tempIntSlice)
 	}
-	for i, result := range results {
-		if i == 1 {
-			continue
-		}
-		for a := range result {
-			if a == 1 {
-				continue
-			}
-			if result[a] >= results[i+1][a] && result[a+1] <= results[i+1][a+1] {
-				return true
-			}
-			if result[a] <= results[i+1][a] && result[a+1] >= results[i+1][a+1] {
-				return true
-			}
-			return false
-		}
+	return
+}
+
+func (s Sections) HasAnyOverlap() bool {
+	if s[0][0] >= s[1][0] && s[0][1] <= s[1][1] ||
+		s[0][0] <= s[1][0] && s[0][1] >= s[1][1] {
+		fmt.Println("first block")
+		return true
+	}
+
+	// a >= c && a <= d || b >= c && b <= d
+	if s[0][0] >= s[1][0] && s[0][0] <= s[1][1] ||
+		s[0][1] >= s[1][0] && s[0][1] <= s[1][1] {
+		fmt.Println("second block")
+		return true
 	}
 	return false
 }
 
-func (d Day4) DoesAnySectionOverlap(line string) bool {
-	sections := strings.Split(line, ",")
-	results := [][]int{}
-	for _, section := range sections {
-		tempStrSlice := strings.Split(section, "-")
-		tempIntSlice := []int{}
-		for _, i := range tempStrSlice {
-			tempInt, _ := strconv.Atoi(i)
-			tempIntSlice = append(tempIntSlice, tempInt)
-		}
-		results = append(results, tempIntSlice)
-	}
-	for i, result := range results {
-		if i == 1 {
-			continue
-		}
-		for a := range result {
-			if a == 1 {
-				continue
-			}
-			//  i          i+1
-			// [a, b] [c, d]
-			// 36-57,7-36
-			b := a + 1
-			c := a
-			d := b
-			if result[a] >= results[i+1][a] && result[a+1] <= results[i+1][a+1] ||
-				result[a] <= results[i+1][a] && result[a+1] >= results[i+1][a+1] {
-				return true
-			}
-
-			if result[a] >= results[i+1][c] && result[a] <= results[i+1][d] ||
-				result[b] >= results[i+1][c] && result[b] <= results[i+1][d] {
-				return true
-			}
-			return false
-		}
+func (s Sections) HasCompleteOverlap() bool {
+	if s[0][0] >= s[1][0] && s[0][1] <= s[1][1] ||
+		s[0][0] <= s[1][0] && s[0][1] >= s[1][1] {
+		return true
 	}
 	return false
 }
